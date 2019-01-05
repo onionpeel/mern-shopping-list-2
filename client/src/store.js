32960df952a0dfc34ccfa1b26243abc5d09d6685
Jeff Dinkelman <mfankenstein@yahoo.com>
2019-01-05 15:23:08 -0800

    Updated store.js so app works in browsers other than chrome, 2nd try

--- client/src/store.js
+++ client/src/store.js
@@ -12,7 +12,7 @@ const middleware = [thunk];
 // ));
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, initialState, composeEnhancers(
+const store = createStore(rootReducer, initialState, composeEnhancers(
   applyMiddleware(...middleware)
 ));
 
